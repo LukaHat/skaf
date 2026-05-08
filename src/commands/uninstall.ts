b2f33124ec9738b84dae9d10b3602494819bccae
lukahat <luka.hat58@gmail.com>
2026-05-08 22:45:58 +0200

    fix(uninstall): move success log inside try and fix error message typo

--- src/commands/uninstall.ts
+++ src/commands/uninstall.ts
@@ -6,11 +6,15 @@ export const uninstallCommand = () => {
   const skafrcPath = join(cwd(), ".skafrc");
 
   if (existsSync(skafrcPath)) {
-    rmSync(skafrcPath);
-    console.log("Removed .skafrc");
+    try {
+      rmSync(skafrcPath);
+      console.log("Removed .skafrc");
+    } catch (error) {
+      console.log("Something went wrong when deleting .skafrc");
+    }
   } else {
     console.log("No .skafrc found in current directory.");
   }
 
-  console.log("To remove the CLI globally, run: npm uninstall -g skaf");
+  console.log("To remove the CLI globally, run: npm uninstall -g skafr");
 };
